8e97f7a81b749e7f9437d6eafb6e9b9373c658a0
Alper Celik <acelik@indocresearch.org>
2018-01-29 09:48:58 -0500

    improvements on totals

--- R/totals.R
+++ R/totals.R
@@ -1,45 +1,39 @@
 totals <- function(data, genedf, plot=F, cores=1) {
 
-    summer <- function(nuc, df) {
-        a <- df[df$nucleotide == nuc, ]
-        a <- sum(a[["freq"]], na.rm = T)
-        a
-    }
-    genes <- genedf$gene
-    get_data <- function(gene_name) {
-        gene <- get_gene(gene = gene_name, genedf=genedf, data = data, seq = NA)
-        if (is.null(gene)) {
-            NULL
-        } else {
-            coord <- genedf[genedf$gene == gene_name, ]
-            gene <- gene[gene$nucleotide >= 0 & gene$nucleotide <= (coord$end - coord$start + 1), ]
-            counts <- sapply(unique(gene$nucleotide), summer, gene)
-            gene <- as.data.frame(cbind(unique(gene[, 1:2]), counts))
-            if (dim(gene)[1] == 0) {
-                NULL
-            } else {
-                dats <- colSums(gene[, 2:3])
-                gene <- data.frame(sum_coverage = dats[1], sum_ribo = dats[2])
-                rownames(gene) <- gene_name
-                gene
-            }
-        }
-    }
-    if(cores>1){
-      totals <- do.call("rbind", mclapply(genes, get_data, mc.cores = cores))
-      if (is.null(data$rna)) {
-        totals$sum_coverage <- NA
+  genes <- genedf$gene
+  get_data <- function(gene_name) {
+    gene <- get_gene(gene = gene_name, genedf=genedf, data = data, seq = NA)
+    if (is.null(gene)) {
+      NULL
+    } else {
+      coord <- genedf[genedf$gene == gene_name, ]
+      gene <- gene[gene$nucleotide >= 0 & gene$nucleotide <= (coord$end - coord$start + 1), ]
+      if (dim(gene)[1] == 0) {
+        NULL
+      } else {
+        gene$frame<-as.numeric(gene$frame)
+        dats <- colSums(gene, na.rm = T)
+        dats<-as.data.frame(t(dats))
+        dats$gene<-gene_name
+        return(dats)
       }
-      }else {
-        totals <- do.call("rbind", lapply(genes, get_data))
-        if (is.null(data$rna)) {
-          totals$sum_coverage <- NA
-        }
-      }
-    totals<-as.data.frame(totals)
-    if(plot){
-      print(ggplot(totals, aes(x=sum_coverage, y=sum_ribo))+geom_point())
     }
-    invisible(totals)
+  }
+  if(cores>1){
+    tots <- do.call("rbind", mclapply(genes, get_data, mc.cores = cores))
+  }else {
+    tots <- do.call("rbind", lapply(genes, get_data))
+  }
+  if(is.null(data$rna)){
+    totals_df<-data.frame(gene=tots$gene, sum_ribo=tots$freq, sum_coverage=NA)
+  } else {
+    totals_df<-data.frame(gene=tots$gene, sum_ribo=tots$freq, sum_coverage=tots$coverage)
+  }
+
+  totals<-as.data.frame(totals)
+  if(plot){
+    print(ggplot(totals_df, aes(x=sum_coverage, y=sum_ribo))+geom_point())
+  }
+  invisible(totals_df)
 }
 
